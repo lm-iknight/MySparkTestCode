3c4a8c300dab991120c6918516481d7d35e54c9f
iknight <iknight@163.com>
2020-02-13 14:05:29 +0800

    添加注释

--- src/main/scala/com/liming/WordCount.scala
+++ src/main/scala/com/liming/WordCount.scala
@@ -12,15 +12,15 @@ object WordCount {
   def main(args: Array[String]): Unit = {
     val config: SparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
     val sc = new SparkContext(config)
-
+    //从文件读取文本
     //    val lines: RDD[String] = sc.textFile("input/manager.log")
+    //用空格对文本进行切分
     //    val words: RDD[String] = lines.flatMap(_.split(" "))
     //    val wordToOne: RDD[(String, Int)] = words.map((_, 1))
     //    val wordToSum: RDD[(String, Int)] = wordToOne.reduceByKey(_ + _)
     //    val result: Array[(String, Int)] = wordToSum.collect()
     //    result.foreach(println)
-    sc.textFile("input/manager.log").flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).collect().foreach(println)
-
+    sc.textFile("input/manager.log").flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _).collect().foreach(println)
 
   }
 }
