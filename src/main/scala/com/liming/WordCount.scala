b05f3aa30d80eddc05d2c54105a51622bd5d3fb3
iknight <iknight@163.com>
2020-02-13 13:26:45 +0800

    第一次提交

---
+++ src/main/scala/com/liming/WordCount.scala
@@ -0,0 +1,26 @@
+package com.liming
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * 功能描述: 
+ * 作者: 李明
+ * 创建时间: 2020年02月13日 13点08分12秒
+ */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val config: SparkConf = new SparkConf().setMaster("local[*]").setAppName("WordCount")
+    val sc = new SparkContext(config)
+
+    //    val lines: RDD[String] = sc.textFile("input/manager.log")
+    //    val words: RDD[String] = lines.flatMap(_.split(" "))
+    //    val wordToOne: RDD[(String, Int)] = words.map((_, 1))
+    //    val wordToSum: RDD[(String, Int)] = wordToOne.reduceByKey(_ + _)
+    //    val result: Array[(String, Int)] = wordToSum.collect()
+    //    result.foreach(println)
+    sc.textFile("input/manager.log").flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).collect().foreach(println)
+
+
+  }
+}
